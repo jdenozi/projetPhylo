209f7454d2c612925e06604ae75a39ddf6bb1318
ed <delanoe.edouard@gmail.com>
2018-11-21 18:52:18 +0100

    corrections parcours

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 import node
 import newickParser
 
-arbregene = newickParser.lectureArbre("test.nwk")
+arbregene = newickParser.lectureArbre("test.txt")
 print(arbregene)
 #index = len(arbregene) - 1
 root = node.node(name="root", enfants=[], gen=0)
 print(root)
-root.enfants=newickParser.creationArbre(root, 0, arbregene)
+root.enfants = newickParser.creationArbre(root, 1, arbregene)
 newickParser.parcoursArbre(root)
 print("fin")
--- newickParser.py
+++ newickParser.py
@@ -4,12 +4,14 @@
 import sys,os,re
 from node import*
 
-index =0
+index = 0
+
+
 def lectureArbre(fichier):
     global index
     with open(fichier) as f:
-        arbre=f.read()
-    index = len(arbre)-2
+        arbre = f.read()
+    index = len(arbre) - 2
     return arbre
 
 
@@ -17,27 +19,33 @@ def creationArbre(parent, niveau, arbre):
     global index
     listeEnfants = []
     strtmp = ""
-    print("debut creation")
-    print (index)
+    print("appel fonction")
     while index >= 0:
+        print ("indice {} niveau {}".format(index, niveau))
+        print (arbre[index])
         #if arbre[index] == ';':
             #index -= 1
             #return (node(gen=0, name="root", enfants=creationArbre(self, 1)))
         if arbre[index] == ')':  # création enfant
             index -= 1
-            listeEnfants.append(node(gen=niveau, parent=parent, enfants=
-            creationArbre(parent, niveau + 1, arbre)))
+            listeEnfants.append(node(name="parenttest", gen=niveau, parent=parent,))
+            listeEnfants[-1].enfants=creationArbre(listeEnfants[-1], niveau+1, arbre)
+            #listeEnfants.append(node(name=parent, gen=niveau, parent=parent, enfants=
+            #creationArbre(parent, niveau+1, arbre)))
         elif arbre[index] == ',':  # création frère(s)
             #parent.enfants[-1].name = strtmp
             strtmp = ""
             index -= 1
-            listeEnfants.append(node(gen=niveau, parent=parent, enfants=
-            creationArbre(parent, niveau + 1, arbre)))
+            print("deuxième enfant")
+            print(parent)
+            #parent.enfants.append(node(gen=niveau, parent=parent, enfants=
+            #creationArbre(parent, niveau + 1, arbre)))
         elif arbre[index] == '(':  # sortie de la fratrie
             #parent.enfants[-1].name = strtmp
             strtmp = ""
             index -= 1
             niveau -= 1
+            print(listeEnfants)
             return(listeEnfants)
         elif arbre[index] == ':':
             #parent.distance = strtmp
@@ -50,6 +58,6 @@ def creationArbre(parent, niveau, arbre):
 
 def parcoursArbre(root):
     print(root)
-    if (not root.isLeaf()): 
+    if (not root.isLeaf()):
         for i in root.enfants:
             parcoursArbre(i)
