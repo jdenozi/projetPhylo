b7dc94d7947e70cb9df37acdae82c63aadd44869
Edouard Delanoe <delanoe.edouard@gmail.com>
2018-12-21 00:15:36 +0100

    new main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#-*- coding Utf-8 -*-
 
+import sys, os, re
 import node
-import newickParser
+import newickparser
 
-arbregene = newickParser.lectureArbre("test.txt")
-print(arbregene)
-#index = len(arbregene) - 1
-root = node.node(name="root", enfants=[], gen=0)
-newickParser.creationArbre(root, 0, arbregene)
-#newickParser.parcoursArbre(root)
+os.system("clear")
+root = node.node(name ="root", gen = 0, enfants = [])
+newickparser.parser(None, root, 0, newickparser.lectureArbre("arbre.nwk").strip())
+#print(newickparser.lectureArbre("arbre.nwk").strip())
 print(root)
-print("fin")
+
+
+
