b54028b6a2e7a9a410bb7a1a3ab99b18952fbb55
Edouard Delanoe <delanoe.edouard@gmail.com>
2018-12-21 00:07:26 +0100

    nouvel arbre

---
+++ node.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env/ python3
+# -*- coding Utf-8 -*-
+
+
+class node:
+    def __init__(self, **kwargs):
+
+        self.name = kwargs.get('name')
+        self.parent = kwargs.get('parent')
+        self.enfants = kwargs.get('enfants')
+        self.gen = kwargs.get('gen')
+        self.dist = kwargs.get('dist')
+    def isRoot(self):
+        if self.parent:
+            return False
+        else:
+            return True
+
+    def isLeaf(self):
+        if self.enfants:
+            return False
+        else:
+            return True
+
+    def __repr__(self):
+                return("nom({}) generation({}) enfants({}) dist({})".format
+                (self.name, self.gen, self.enfants, self.dist))
+
+    def showTree(self):
+        if self.enfants:
+            for enfant in self.enfants:
+                print (enfant.name+", "+str(enfant.gen))
+                enfant.showTree
